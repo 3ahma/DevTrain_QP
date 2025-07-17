03c1fe4b7ec5e3d05f056bcb5a740d0a90464110
3ahma <hussinw70@gmail.com>
2025-07-17 16:49:38 +0300

    did some arrangements

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -1,28 +1,4 @@
-terraform {
-  required_providers {
-    local = {
-      source  = "hashicorp/local"
-      version = "~> 2.0"
-    }
-  }
-}
-
-
-provider "local" {}
-
-module "my_file"{
+module "prod"{
     source       = "../../modules"
     file_content = data.terraform_remote_state.dev.outputs.file_content
-}
-
-variable "file_content" {
-  description = "String in the file"
-  type        = string 
-}
-
-data "terraform_remote_state" "dev" {
-  backend = "local"
-  config = {
-    path = "../dev/terraform.tfstate"
-  }
 }
--- modules/main.tf
+++
@@ -1,5 +0,0 @@
-
-resource "local_file" "hello_file" {
-  filename = "${path.root}/../../hello.txt"
-  content  = var.file_content
-}
